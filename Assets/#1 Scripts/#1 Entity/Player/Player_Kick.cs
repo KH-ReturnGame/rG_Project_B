a76d30d085c07ffd2e13f9245d97d95041d5464f
이형주 <jujumom630@gmail.com>
2024-07-30 12:07:00 +0900

    player kick스크립트 생성

---
+++ Assets/#1 Scripts/#1 Entity/Player/Player_Kick.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Kick : MonoBehaviour
+{
+    private Player _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
