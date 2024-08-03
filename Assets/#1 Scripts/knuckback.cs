36ff0e61c6b1be31b6587f3265514572c5e68373
mugglewoojin <mugglewoojin@gmail.com>
2024-08-03 21:45:36 +0900

    에너미 맵이탈 방지

--- Assets/#1 Scripts/knuckback.cs
+++ Assets/#1 Scripts/knuckback.cs
@@ -11,6 +11,7 @@ public class knuckback : MonoBehaviour
     float X, Y;
     float knuckbackpower = 100;
     int dashdirection;//1위2왼3아래4오른
+    int Xdirection, Ydirection;
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
@@ -25,43 +26,64 @@ public class knuckback : MonoBehaviour
         Y = MyY - OtherY;
         if (Mathf.Abs(X) - Mathf.Abs(Y) >= 0 && X >= 0)
         {
-            dashdirection = 2;
+            Xdirection = 1;
         }
         else if (Mathf.Abs(X) - Mathf.Abs(Y) >= 0 && X < 0)
         {
-            dashdirection = 4;
+            Xdirection = -1;
         }
         else if (Mathf.Abs(X) - Mathf.Abs(Y) < 0 && Y >= 0)
         {
-            dashdirection = 3;
+            Ydirection = 1;
         }
         else if (Mathf.Abs(X) - Mathf.Abs(Y) < 0 && Y < 0)
         {
-            dashdirection = 1;
+            Ydirection = -1;
         }
         //Debug.Log("direction: " + dashdirection);
-        Debug.Log(dashdirection);
+        //Debug.Log(dashdirection);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 HorizontalVec = new Vector2(rigid.position.x + Xdirection, rigid.position.y);
+        Debug.DrawRay(HorizontalVec, Vector2.down, new Color(0, 1, 0));
+        RaycastHit2D HrayHit = Physics2D.Raycast(HorizontalVec, Vector2.down, 1, LayerMask.GetMask("platform"));
+        Vector2 VerticalVec = new Vector2(rigid.position.x, rigid.position.y + Ydirection);
+        Debug.DrawRay(VerticalVec, Vector2.left, new Color(0, 1, 0));
+        RaycastHit2D VrayHit = Physics2D.Raycast(VerticalVec, Vector2.left, 1, LayerMask.GetMask("platform"));
+        Vector2 MHorizontalVec = new Vector2(rigid.position.x + Xdirection, rigid.position.y);
+        Debug.DrawRay(MHorizontalVec, Vector2.down, new Color(0, 1, 0));
+        RaycastHit2D MHrayHit = Physics2D.Raycast(MHorizontalVec, Vector2.down, 1, LayerMask.GetMask("platform"));
+        Vector2 MVerticalVec = new Vector2(rigid.position.x, rigid.position.y + Ydirection);
+        Debug.DrawRay(MVerticalVec, Vector2.left, new Color(0, 1, 0));
+        RaycastHit2D MVrayHit = Physics2D.Raycast(MVerticalVec, Vector2.left, 1, LayerMask.GetMask("platform"));
+        if (HrayHit.collider == null || VrayHit.collider == null || MHrayHit.collider == null || MVrayHit.collider == null)
+        {
+            rigid.velocity = new Vector2(0, 0);
+            Xdirection = Ydirection = 0;
+        }
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "ground")
         {
-            if (dashdirection == 1)
+            if (Ydirection == -1)
             {
                 rigid.AddForce(Vector2.down * knuckbackpower, ForceMode2D.Impulse);
                 Debug.Log("asdf");
             }
-            else if (dashdirection == 2)
+            else if (Xdirection == 1)
             {
                 rigid.AddForce(Vector2.right * knuckbackpower, ForceMode2D.Impulse);
                 Debug.Log("asdf");
             }
-            else if (dashdirection == 3)
+            else if (Ydirection == 1)
             {
                 rigid.AddForce(Vector2.up * knuckbackpower, ForceMode2D.Impulse);
                 Debug.Log("asdf");
             }
-            else if (dashdirection == 4)
+            else if (Xdirection == -1)
             {
                 rigid.AddForce(Vector2.left * knuckbackpower, ForceMode2D.Impulse);
                 Debug.Log("asdf");
