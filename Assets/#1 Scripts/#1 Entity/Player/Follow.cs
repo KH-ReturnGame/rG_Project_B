fada55feb37293d5b4ab2db062cc5aea4bb451c0
이형주 <jujumom630@gmail.com>
2024-08-12 08:20:37 +0900

    반지름 제한

--- Assets/#1 Scripts/#1 Entity/Player/Follow.cs
+++ Assets/#1 Scripts/#1 Entity/Player/Follow.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
     public SpriteRenderer _playerfilp;
+    public Transform _playerTransform;
     private SpriteRenderer spriteRenderer;
+    public float radius;
     void Start()
     {
         spriteRenderer = this.GetComponent<SpriteRenderer>();
@@ -18,10 +20,24 @@ public class Follow : MonoBehaviour
         // Z 축의 값을 고정합니다. 2D이기 때문에 Z 축은 0으로 설정합니다.
         mousePosition.z = 0;
 
-        // 오브젝트의 위치를 마우스 위치로 설정합니다.
-        transform.position = mousePosition;
-        
         // 플레이어 플립이랑 방향 똑같이
         spriteRenderer.flipX = _playerfilp.flipX;
+
+         // 플레이어와의 거리 계산
+        Vector3 direction = mousePosition - _playerTransform.position;
+        float distance = direction.magnitude;
+
+        // 최대 거리를 초과할 경우
+        if (distance > radius)
+        {
+            // 최대 거리 내에서 마우스를 따라가도록 위치를 조정
+            direction = direction.normalized; // 방향 벡터를 정규화
+            transform.position = _playerTransform.position + direction * radius;
+        }
+        else
+        {
+            // 최대 거리를 초과하지 않으면 마우스를 그대로 따라갑니다.
+            transform.position = mousePosition;
+        }
     }
 }
