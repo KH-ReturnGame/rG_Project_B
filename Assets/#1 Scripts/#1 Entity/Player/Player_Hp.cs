68a1214efc9d23f3275f1d6e6a104885e1bc2d77
20617chaehun <yunchaehun0122@naver.com>
2024-09-20 14:40:56 +0900

    죽었을 때 retry 버튼 활성화까지
    아쉬운건 씬 로드는 아직 안됨

--- Assets/#1 Scripts/#1 Entity/Player/Player_Hp.cs
+++ Assets/#1 Scripts/#1 Entity/Player/Player_Hp.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player_Hp : MonoBehaviour
 {
     Player _player;
     Image _PlayerHpUI;
+    public Button Retry_UI;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +16,18 @@ public class Player_Hp : MonoBehaviour
         _PlayerHpUI = GameObject.Find("Player_HP_UI").GetComponent<Image>();
         _PlayerHpUI.fillAmount = 1;
         StartCoroutine(DecreaseHPOverTime());
+        Retry_UI.gameObject.SetActive(false);
+        Retry_UI.onClick.AddListener(RetryGame);
+            
     }
 
     // Update is called once per frame
     void Update()
     {
         _PlayerHpUI.fillAmount = _player._currentHp / _player._maxHp;
+
+        if (_PlayerHpUI.fillAmount == 0)
+            Retry_UI.gameObject.SetActive(true);
     }
 
     IEnumerator DecreaseHPOverTime()
@@ -30,5 +38,11 @@ public class Player_Hp : MonoBehaviour
             yield return null;
             yield return new WaitForSeconds(0.02f);
         }
+    }
+    
+    void RetryGame()
+    {
+        // 현재 씬을 다시 로드
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
