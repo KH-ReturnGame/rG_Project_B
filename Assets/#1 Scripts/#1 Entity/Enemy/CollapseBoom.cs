41cec392c2b9adc9f42b9f65ba593644d0ae7c36
이형주 <jujumom630@gmail.com>
2024-11-12 15:01:42 +0900

    시체 프리팹 및 머테리얼 추가

---
+++ Assets/#1 Scripts/#1 Entity/Enemy/CollapseBoom.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollapseBoom : MonoBehaviour
+{
+    public GameObject[] Collapses;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
