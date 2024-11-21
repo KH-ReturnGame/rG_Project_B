64fa6939f8d3c1f8e36b73cd5d18c0d1df9459e1
Pjh08502 <parkjh080502@naver.com>
2024-11-22 00:53:59 +0900

    근접에너미 스크립트 다시만들기
    +실행되나 테스트까지

--- Assets/#1 Scripts/#1 Entity/Enemy/Enemy_closer.cs
+++ Assets/#1 Scripts/#1 Entity/Enemy/Enemy_closer.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestEnemyclose : MonoBehaviour
+public class Enemy_closer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello unity");
     }
 
     // Update is called once per frame
