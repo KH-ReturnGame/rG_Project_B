4538f6d92cda2149eb3079133fef001cd059c4a6
이형주 <jujumom630@gmail.com>
2024-12-17 15:42:04 +0900

    애니메이터 준비

--- Assets/#1 Scripts/#1 Entity/Enemy/turret/turret.cs
+++ Assets/#1 Scripts/#1 Entity/Enemy/turret/turret.cs
@@ -23,11 +23,11 @@ public class turret : MonoBehaviour
 
     IEnumerator Fire()
     {
-        // if (_enemy.IsContainState(EnemyStates.IsDie)) // 적이 사망했는지 확인
-        // {
-        //     yield break; // 코루틴 종료
-        // }
-        // 총알 발사
+        if (_enemy.IsContainState(EnemyStates.IsDie)) // 적이 사망했는지 확인
+        {
+            yield break; // 코루틴 종료
+        }
+        
         Instantiate(bulletPrefab, new Vector3(transform.position.x + (fire_direc * 1.2f), transform.position.y, transform.position.z), transform.rotation);
         
         yield return new WaitForSeconds(1.5f); // 1.5초 대기 후 반복
