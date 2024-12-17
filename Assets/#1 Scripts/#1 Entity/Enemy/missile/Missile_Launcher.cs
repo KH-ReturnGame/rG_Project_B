a72b875d98a8428784207cdde8fab56dbbd281de
이형주 <jujumom630@gmail.com>
2024-12-18 00:18:39 +0900

    중간저장
    아 씹 어지러워

--- Assets/#1 Scripts/#1 Entity/Enemy/missile/Missile_Launcher.cs
+++ Assets/#1 Scripts/#1 Entity/Enemy/missile/Missile_Launcher.cs
@@ -10,13 +10,11 @@ public class Missile_Launcher : MonoBehaviour
     public Vector3 summon_pos;
     private SpriteRenderer _flip;
     public bool isGT;
-    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
         enemy = GetComponent<Enemy>();
         _flip = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
         StartCoroutine(Fire());
     }
 
@@ -30,7 +28,7 @@ public class Missile_Launcher : MonoBehaviour
     {
         if(!enemy.IsContainState(EnemyStates.IsDie))
         {
-            animator.SetTrigger("attack");
+            enemy.AddState(EnemyStates.IsAttacking);
             Vector3 direc = isFlip();
             Quaternion _rotation = Rotate();
 
