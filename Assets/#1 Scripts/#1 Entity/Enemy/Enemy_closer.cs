5c01621f18acc2b7daa102a1db52267e02639b34
Pjh08502 <164426025+Pjh08502@users.noreply.github.com>
2024-12-26 23:29:29 +0900

    Update Enemy_closer.cs
    추적 따오기

--- Assets/#1 Scripts/#1 Entity/Enemy/Enemy_closer.cs
+++ Assets/#1 Scripts/#1 Entity/Enemy/Enemy_closer.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Enemy_closer : MonoBehaviour
+public class Enemy_PlayerChase : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float speed = 3f; // 이동속도
+    private Transform enemy; //에너미 위치변수
+    private Transform player; //플레이어 위치변수
     void Start()
     {
-        Debug.Log("Hello unity");
+        enemy = this.gameObject.GetComponent<Transform>();
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+       
+        ChasingPlayer();
+        Debug.Log(player.position);
+    }
+    private void ChasingPlayer()
+    {
+        transform.position = Vector2.MoveTowards(enemy.position, player.position, speed * Time.deltaTime); //플레이어한테 이동
     }
 }
