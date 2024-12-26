2ad73208824c41c480220bb106617539878e266d
20617chaehun <yunchaehun0122@naver.com>
2024-12-26 19:03:29 +0900

    색깔과 구간 시간 추가
    .

--- Assets/#1 Scripts/#1 Entity/Player/PlayerDash.cs
+++ Assets/#1 Scripts/#1 Entity/Player/PlayerDash.cs
@@ -14,10 +14,9 @@ public class PlayerDash : MonoBehaviour
     public float radius;
     private bool isCanDash;
     public Animator animator;
-    
+
     void Awake()
     {
-        
         spriteRenderer = GetComponent<SpriteRenderer>();
         _player = this.transform.parent.GetComponent<Player>();
 
@@ -31,69 +30,53 @@ public class PlayerDash : MonoBehaviour
 
         isCanDash = true;
     }
+
     void Update()
     {
-       
-        // 마우스의 월드 좌표를 얻어옵니다.
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        // Z 축의 값을 고정합니다. 2D이기 때문에 Z 축은 0으로 설정합니다.
         mousePosition.z = 0;
-
-        // 플레이어 플립이랑 방향 똑같이
         spriteRenderer.flipX = _playerfilp.flipX;
 
-         // 플레이어와의 거리 계산
         Vector3 direction = mousePosition - _playerTransform.position;
         float distance = direction.magnitude;
 
-        // 최대 거리를 초과할 경우
         if (distance > radius)
         {
-            // 최대 거리 내에서 마우스를 따라가도록 위치를 조정
-            direction = direction.normalized; // 방향 벡터를 정규화
+            direction = direction.normalized;
             transform.position = _playerTransform.position + direction * radius;
         }
         else
         {
-            // 최대 거리를 초과하지 않으면 마우스를 그대로 따라갑니다.
             transform.position = mousePosition;
         }
 
         RaycastHit2D[] hits = Physics2D.RaycastAll(_playerTransform.position, (transform.position - _playerTransform.position).normalized, distance);
         Debug.DrawLine(_playerTransform.position, transform.position, Color.red);
-        
+
         List<GameObject> hitObjects = new List<GameObject>();
         foreach (var hit in hits)
         {
             hitObjects.Add(hit.collider.gameObject);
         }
 
-        // 리스트에 ground 태그를 가진 오브젝트가 있는지 확인
         bool groundHit = hitObjects.Exists(obj => obj.CompareTag("ground"));
         if (groundHit)
         {
-                RaycastHit2D groundRaycast = Array.Find(hits, hit => hit.collider != null && hit.collider.CompareTag("ground"));
-
-                // ground 오브젝트와 충돌한 경우, transform의 위치를 조정하여 땅을 넘지 않도록 한다.
-                Vector3 hitPoint = groundRaycast.point; // 충돌한 지점
-                Vector3 normal = groundRaycast.normal; // 충돌한 표면의 법선 벡터
-
-                // 충돌 지점과 플레이어 위치 사이의 벡터 계산
-                Vector3 fromPlayerToHit = hitPoint - _playerTransform.position;
-
-                // 땅을 넘지 않도록 충돌 지점 바로 앞에 위치를 설정
-                transform.position = hitPoint + normal * 0.25f; // 땅을 넘지 않게 약간 떨어진 위치로 설정
+            RaycastHit2D groundRaycast = Array.Find(hits, hit => hit.collider != null && hit.collider.CompareTag("ground"));
+            Vector3 hitPoint = groundRaycast.point;
+            Vector3 normal = groundRaycast.normal;
+            Vector3 fromPlayerToHit = hitPoint - _playerTransform.position;
+            transform.position = hitPoint + normal * 0.25f;
         }
 
-        lineRenderer.SetPosition(0, _playerTransform.position); // 첫 번째 점 (플레이어 위치)
-        lineRenderer.SetPosition(1, transform.position); // 두 번째 점 (팔로우 오브젝트 위치)
+        lineRenderer.SetPosition(0, _playerTransform.position);
+        lineRenderer.SetPosition(1, transform.position);
 
-        if(Input.GetMouseButtonDown(0) && isCanDash)
+        if (Input.GetMouseButtonDown(0) && isCanDash)
         {
             _playerMovement.DragonDash();
-            
-            if(groundHit)
+
+            if (groundHit)
             {
                 _player.AddState(PlayerStates.IsWall);
                 _playerMovement._playerRigidbody.gravityScale = 0f;
@@ -102,24 +85,17 @@ public class PlayerDash : MonoBehaviour
             bool enemyHit = hitObjects.Exists(obj => obj.CompareTag("Enemy"));
             if (enemyHit)
             {
-                // 여기에 적과 충돌한 경우 처리할 로직 추가
-
-                // 예시: 적에게 데미지를 주는 메서드 호출 (적 오브젝트와의 상호작용)
                 RaycastHit2D enemyRaycast = Array.Find(hits, hit => hit.collider != null && hit.collider.CompareTag("Enemy"));
                 if (enemyRaycast.collider != null)
                 {
-                    // 여기서 적과의 상호작용을 처리할 수 있습니다.
                     Enemy _enemy = enemyRaycast.collider.GetComponent<Enemy>();
                     if (_enemy != null)
                     {
-                        _enemy.TakeDamage(50); // 예시로 적에게 데미지를 주는 메서드 호출
+                        _enemy.TakeDamage(50);
                         _player.RecoveryHp(15);
                     }
                 }
             }
-            
-            
         }
     }
 }
-
--- Assets/#1 Scripts/#1 Entity/Player/Player_Movement.cs
+++ Assets/#1 Scripts/#1 Entity/Player/Player_Movement.cs
@@ -1,34 +1,25 @@
 using System.Collections;
 using UnityEngine;
 
-/// <summary>
-/// 플레이어 움직임 담당 클래스
-/// </summary>
-/// <returns></returns>
 public class Player_Movement : MonoBehaviour
 {
-    //플레이어
     public Rigidbody2D _playerRigidbody;
     private Player _player;
     private SpriteRenderer spriteRenderer;
     private Collider2D _playerCollider;
 
-    //기본 좌우, 점프
     private float _recentDirection = 1;
     [SerializeField] private float _movementSpeed = 8.00f;
     private float _movementInputDirection;
     public float _jumpForce = 12.00f;
     [SerializeField] private TrailRenderer tr;
 
-    //공격 대쉬
     public GameObject WhereToDash;
     public float ghostDelay;
-
     private float ghostDelaySeconds;
     public GameObject ghost;
     public bool makeGhost = false;
 
-    //제일 처음 호출
     void Start()
     {
         ghostDelaySeconds = ghostDelay;
@@ -39,10 +30,8 @@ public class Player_Movement : MonoBehaviour
         _playerCollider = GetComponent<Collider2D>();
     }
 
-    //매 프레임 실행
     void Update()
     {
-        //가로 입력 체크하기 --------------------------------------------------------------------------------
         _movementInputDirection = Input.GetAxisRaw("Horizontal");
         if (!_player.IsContainState(PlayerStates.IsWall))
         {
@@ -51,23 +40,17 @@ public class Player_Movement : MonoBehaviour
                 _recentDirection = _movementInputDirection;
             }
 
-            // 방향전환
             if (_recentDirection != 0)
             {
                 spriteRenderer.flipX = _recentDirection != 1;
             }
         }
 
-        //점프 코드 --------------------------------------------------------------------------------
-        //땅에 있을때, 벽에 있을때
-        if (Input.GetButtonDown("Jump")
-            && _player.IsContainState(PlayerStates.CanJump)
-            && !_player.IsContainState(PlayerStates.IsWall))
+        if (Input.GetButtonDown("Jump") && _player.IsContainState(PlayerStates.CanJump) && !_player.IsContainState(PlayerStates.IsWall))
         {
             Jump();
         }
 
-        // 벽슬라이드, 벽 점프 --------------------------------------------------------------------------------
         if (_player.IsContainState(PlayerStates.IsWall))
         {
             _playerRigidbody.velocity = new Vector2(0, 0);
@@ -77,7 +60,6 @@ public class Player_Movement : MonoBehaviour
             }
         }
 
-        // CanJump 상태 관리 ------------------------------------------------------------------------------
         if (_player.IsContainState(PlayerStates.IsGround) || _player.IsContainState(PlayerStates.IsWall))
         {
             _player.AddState(PlayerStates.CanJump);
@@ -87,7 +69,6 @@ public class Player_Movement : MonoBehaviour
             _player.RemoveState(PlayerStates.CanJump);
         }
 
-        // 대쉬 상태 관리 ------------------------------------------------------------------------------
         if (Input.GetKeyDown(KeyCode.LeftControl) && !_player.IsContainState(PlayerStates.IsDragon))
         {
             _player.AddState(PlayerStates.IsDragon);
@@ -100,19 +81,15 @@ public class Player_Movement : MonoBehaviour
         }
     }
 
-
-    //0.02초마다 실행  --------------------------------------------------------------------------------
     private void FixedUpdate()
     {
-        //움직임 적용
         if (_movementInputDirection != 0)
         {
             ApplyMovement();
         }
         else if (!_player.IsContainState(PlayerStates.IsWallJumping))
         {
-            _playerRigidbody.velocity =
-                new Vector2(_movementInputDirection * _movementSpeed, _playerRigidbody.velocity.y);
+            _playerRigidbody.velocity = new Vector2(_movementInputDirection * _movementSpeed, _playerRigidbody.velocity.y);
         }
     }
 
@@ -130,7 +107,6 @@ public class Player_Movement : MonoBehaviour
     {
         if (_player.IsContainState(PlayerStates.IsGround))
         {
-            _playerRigidbody.velocity = new Vector2(_playerRigidbody.velocity.x, 0);
             _playerRigidbody.velocity = new Vector2(_playerRigidbody.velocity.x, _jumpForce);
         }
     }
@@ -139,59 +115,30 @@ public class Player_Movement : MonoBehaviour
     {
         if (_player.IsContainState(PlayerStates.IsWall))
         {
-            Vector2 rayDirection = Vector3.up; // Ray 방향 (위쪽 방향)
-            int layerMask = 1 << 6;
-            RaycastHit2D hit;
-
-            Debug.DrawRay(transform.position, rayDirection * 1f, Color.green);
-
-            hit = Physics2D.Raycast(transform.position, rayDirection, 1f, layerMask);
-            if (hit.collider != null)
-            {
-                // 위로 점프
-                _playerRigidbody.AddForce(Vector2.down * 7.5f, ForceMode2D.Impulse);
-
-                _player.RemoveState(PlayerStates.IsWall);
-                StartCoroutine(DisableMovementForSeconds(0.1f));
-            }
-            else
-            {
-                // 위로 점프
-                _playerRigidbody.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
-
-                // 벽에서 반대 방향으로 힘을 줌
-                _playerRigidbody.AddForce(new Vector2(-_recentDirection * 5, 0), ForceMode2D.Impulse);
-
-                // 방향 전환
-                _recentDirection = -_recentDirection; // 최근 방향을 반대로 설정
-                spriteRenderer.flipX = _recentDirection != 1; // 스프라이트의 방향을 전환
-
-                _player.RemoveState(PlayerStates.IsWall);
-                StartCoroutine(DisableMovementForSeconds(0.15f));
-            }
+            _playerRigidbody.AddForce(new Vector2(-_recentDirection * 5, 10), ForceMode2D.Impulse);
+            _player.RemoveState(PlayerStates.IsWall);
+            StartCoroutine(DisableMovementForSeconds(0.15f));
         }
     }
 
     private IEnumerator DisableMovementForSeconds(float seconds)
     {
-        _player.AddState(PlayerStates.IsWallJumping); // 움직임 비활성화
-        yield return new WaitForSeconds(seconds); // 지정된 시간 동안 대기
-        _player.RemoveState(PlayerStates.IsWallJumping); // 다시 움직일 수 있도록 활성화
+        _player.AddState(PlayerStates.IsWallJumping);
+        yield return new WaitForSeconds(seconds);
+        _player.RemoveState(PlayerStates.IsWallJumping);
     }
 
-    public void DragonDash() // 대쉬하는거
+    public void DragonDash()
     {
         this.makeGhost = true;
         if (makeGhost)
         {
-
             if (ghostDelaySeconds > 0)
             {
                 ghostDelaySeconds -= Time.deltaTime;
             }
             else
             {
-                //Generate a ghost
                 DragonDashSegmented();
                 ghostDelaySeconds = ghostDelay;
                 this.makeGhost = false;
@@ -206,22 +153,23 @@ public class Player_Movement : MonoBehaviour
 
     public void DragonDashSegmented()
     {
-        // 1) 시작 위치와 목표 위치
+        StartCoroutine(CreateGhostSegments());
+    }
+
+    private IEnumerator CreateGhostSegments()
+    {
         Vector2 startPos = transform.position;
         Vector2 endPos = WhereToDash.transform.position;
-
-        // 2) 잔상을 몇 개 찍을지 (5개)
         int segmentCount = 5;
 
-        // 3) for 문을 돌면서, 각 분할 지점에 잔상 생성
         for (int i = 1; i <= segmentCount; i++)
         {
-            float t = i / (float)segmentCount; // 1/5, 2/5, 3/5, 4/5, 5/5
+            float t = i / (float)segmentCount;
             Vector2 spawnPos = Vector2.Lerp(startPos, endPos, t);
 
-            // 잔상 프리팹 생성
             GameObject currentGhost = Instantiate(ghost, spawnPos, transform.rotation);
+            Destroy(currentGhost, 0.3f);
+            yield return new WaitForSeconds(0.025f);
         }
     }
 }
-
